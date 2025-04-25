100e1c0c72801d4f7777bce2c7a3f9da0661a8f9
Rik Huijzer <github@huijzer.xyz>
2025-04-25 09:41:28 +0200

    feat: compress backup (#12)
    Follow up on #11.
    
    Closes #7.

--- fx/src/api.rs
+++ fx/src/api.rs
@@ -11,8 +11,10 @@ use axum::http::header::HeaderMap;
 use axum::http::header::HeaderValue;
 use axum::routing::get;
 use serde_json::json;
+use std::io::Read;
 use tar::Builder;
 use tar::Header;
+use xz2::read::XzEncoder;
 
 async fn get_api(State(ctx): State<ServerContext>) -> Response<Body> {
     let domain = &ctx.args.domain;
@@ -68,11 +70,48 @@ fn error(ctx: &ServerContext, status: StatusCode, message: &str) -> Response<Bod
         "message": message,
     })
     .to_string();
-    response_json(status, body, &ctx)
+    response_json(status, body, ctx)
 }
 
 fn unauthorized(ctx: &ServerContext) -> Response<Body> {
     error(ctx, StatusCode::UNAUTHORIZED, "unauthorized")
+}
+
+struct SiteData<'a> {
+    posts: &'a [Post],
+}
+
+fn create_archive(site_data: &SiteData) -> Vec<u8> {
+    let mut ar = Builder::new(Vec::new());
+
+    for post in site_data.posts {
+        let mut header = Header::new_gnu();
+        header.set_size(post.content.len() as u64);
+        let path = format!("post/{}.md", post.id);
+        header.set_path(&path).unwrap();
+        // Without this, the file is not even readable by the user.
+        header.set_mode(0o644);
+        // Using `+++` for the frontmatter because that is toml in Hugo.
+        let content = indoc::formatdoc! {"
+            +++
+            created = {}
+            updated = {}
+            +++
+
+            {}
+        ", post.created, post.updated, post.content};
+        let data = content.as_bytes();
+        ar.append_data(&mut header, &path, data).unwrap();
+    }
+
+    ar.into_inner().unwrap()
+}
+
+fn compress(data: &[u8]) -> Vec<u8> {
+    let mut compressor = XzEncoder::new(data, 6);
+    let mut compressed = Vec::new();
+    compressor.read_to_end(&mut compressed).unwrap();
+    compressed
 }
 
 async fn get_download_all(State(ctx): State<ServerContext>, headers: HeaderMap) -> Response<Body> {
@@ -90,21 +129,9 @@ async fn get_download_all(State(ctx): State<ServerContext>, headers: HeaderMap) 
             "failed to list posts",
         );
     };
-
-    let mut ar = Builder::new(Vec::new());
-
-    for post in posts {
-        let mut header = Header::new_gnu();
-        header.set_size(post.content.len() as u64);
-        let path = format!("post/{}.md", post.id);
-        header.set_path(&path).unwrap();
-        // Without this, the file is not even readable by the user.
-        header.set_mode(0o644);
-        ar.append_data(&mut header, &path, post.content.as_bytes())
-            .unwrap();
-    }
-
-    let body = ar.into_inner().unwrap();
+    let site_data = SiteData { posts: &posts };
+    let data = create_archive(&site_data);
+    let body = compress(&data);
     let mut headers = HeaderMap::new();
     headers.insert(
         "Content-Type",
@@ -117,5 +144,5 @@ pub fn routes(router: &Router<ServerContext>) -> Router<ServerContext> {
     router
         .clone()
         .route("/api", get(get_api))
-        .route("/api/download/all.tar.gz", get(get_download_all))
+        .route("/api/download/all.tar.xz", get(get_download_all))
 }
--- fx/tests/api_integration.rs
+++ fx/tests/api_integration.rs
@@ -3,12 +3,16 @@ mod common;
 use axum::body::Body;
 use axum::extract::Request;
 use axum::http::StatusCode;
+use chrono::Utc;
 use common::*;
 use fx::serve::app;
 use http_body_util::BodyExt;
 use std::io::Cursor;
+use std::io::Read;
 use tar::Archive;
+use tar::Entry;
 use tower::util::ServiceExt;
+use xz2::read::XzDecoder;
 
 #[tokio::test]
 async fn test_api() {
@@ -35,15 +39,34 @@ pub async fn request_body_authenticated(uri: &str) -> (StatusCode, Vec<u8>) {
 
 #[tokio::test]
 async fn test_download_all() {
-    let (status, body) = request_body_authenticated("/api/download/all.tar.gz").await;
+    let uri = "/api/download/all.tar.xz";
+    let (status, body) = request_body_authenticated(uri).await;
     assert_eq!(status, StatusCode::OK);
 
-    let cursor = Cursor::new(body);
-    let mut ar = Archive::new(cursor);
-    let entries = ar.entries().unwrap();
-    let entries = entries.collect::<Vec<_>>();
-    assert_eq!(entries.len(), 2);
+    let body = Cursor::new(body);
+    let decompressed = XzDecoder::new(body);
+    let mut ar = Archive::new(decompressed);
+    // Do not collect the entries because it moves the stream pointer.
+    let mut entries = ar.entries().unwrap();
+    fn path<T: std::io::Read>(entry: &Entry<T>) -> String {
+        entry.path().unwrap().to_str().unwrap().to_string()
+    }
+    let mut first = entries.next().unwrap().unwrap();
     // SQLite is 1-indexed.
-    assert!(entries[0].as_ref().unwrap().path().unwrap().to_str().unwrap().contains("post/1.md"));
-    assert!(entries[1].as_ref().unwrap().path().unwrap().to_str().unwrap().contains("post/2.md"));
+    assert!(path(&first).contains("post/1.md"));
+
+    let mut content = String::new();
+    first.read_to_string(&mut content).unwrap();
+    let lines = content.lines().collect::<Vec<_>>();
+    assert_eq!(lines[0], "+++");
+    let today = Utc::now().format("%Y-%m-%d").to_string();
+    assert!(lines[1].contains(&format!("created = {today}")));
+    assert!(lines[2].contains(&format!("updated = {today}")));
+    assert_eq!(lines[3], "+++");
+    assert_eq!(lines[4], "");
+    assert!(lines[5].contains("Lorem"));
+
+    let second = entries.next().unwrap().unwrap();
+    assert!(path(&second).contains("post/2.md"));
+    assert!(entries.next().is_none());
 }
