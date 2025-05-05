f33217ab5d63198cf41cf6d78791ea5fbd11a82a
Rik Huijzer <github@huijzer.xyz>
2025-05-05 12:21:10 +0200

    cleanup

--- fx/src/search.rs
+++ fx/src/search.rs
@@ -1,73 +1,57 @@
 //! Search at `/search`.
+use crate::data::Post;
+use crate::data::SqliteDateTime;
 use crate::html::PageSettings;
 use crate::html::Top;
 use crate::html::page;
+use crate::html::post_to_html;
 use crate::serve::ServerContext;
 use crate::serve::content_type;
 use crate::serve::is_logged_in;
-use crate::serve::not_found;
 use crate::serve::response;
 use axum::Router;
 use axum::body::Body;
-use axum::extract::Form;
-use axum::extract::Multipart;
-use axum::extract::Path;
 use axum::extract::Query;
 use axum::extract::State;
 use axum::http::HeaderMap;
 use axum::http::Response;
 use axum::http::StatusCode;
 use axum::routing::get;
-use axum::routing::post;
 use axum_extra::extract::CookieJar;
-use bytes::Bytes;
-use rusqlite::Connection;
 use rusqlite::Result;
-use rusqlite::params;
 use serde::Deserialize;
 use serde::Serialize;
-use sha2::Digest;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct SearchForm {
     pub q: Option<String>,
 }
 
-fn search_form() -> &'static str {
-    "
-    <form action='/search' method='get'>
-        <input type='text' name='q' />
-        <button type='submit'>Search</button>
-    </form>
-    "
+fn search_form(q: &str) -> String {
+    format!(
+        "
+        <form action='/search' method='get'>
+            <input type='text' name='q' value='{q}' />
+            <button type='submit'>Search</button>
+        </form>
+        "
+    )
 }
 
-async fn search_page(ctx: &ServerContext, jar: CookieJar) -> Response<Body> {
-    let is_logged_in = is_logged_in(&ctx, &jar);
-    let mut headers = HeaderMap::new();
-    content_type(&mut headers, "text/html");
-    let title = "Search";
-    let extra_head = "";
-    let settings = PageSettings::new(&title, is_logged_in, false, Top::GoHome, extra_head);
-    let body = search_form();
-    let body = page(&ctx, &settings, &body).await;
-    response(StatusCode::OK, headers, body, &ctx)
-}
-
-struct SearchResult {
-    id: i64,
-    content: String,
-}
-
-async fn search(ctx: &ServerContext, q: &str) -> Vec<SearchResult> {
+async fn search(ctx: &ServerContext, q: &str) -> Vec<Post> {
+    if q.is_empty() {
+        return vec![];
+    }
     let conn = ctx.conn().await;
 
     let stmt = "BEGIN TRANSACTION";
     conn.execute(stmt, []).unwrap();
 
+    // Not copying updated since it's not shown in the preview.
     let stmt = "
         CREATE VIRTUAL TABLE posts_fts USING fts5(
             id,
+            created,
             content,
             content=posts,
             tokenize=trigram
@@ -76,8 +60,8 @@ async fn search(ctx: &ServerContext, q: &str) -> Vec<SearchResult> {
     conn.execute(stmt, []).unwrap();
 
     let stmt = "
-        INSERT INTO posts_fts (id, content)
-        SELECT id, content FROM posts;
+        INSERT INTO posts_fts (id, created, content)
+        SELECT id, created, content FROM posts;
     ";
     conn.execute(stmt, []).unwrap();
 
@@ -87,8 +71,14 @@ async fn search(ctx: &ServerContext, q: &str) -> Vec<SearchResult> {
     let results = results
         .query_map([q], |row| {
             let id: i64 = row.get("id")?;
+            let created: String = row.get("created")?;
             let content: String = row.get("content")?;
-            let post = SearchResult { id, content };
+            let post = Post {
+                id,
+                created: SqliteDateTime::from_sqlite(&created),
+                updated: SqliteDateTime::from_sqlite(&created),
+                content,
+            };
             Ok(post)
         })
         .unwrap()
@@ -107,37 +97,34 @@ async fn get_search(
     search_query: Query<SearchForm>,
 ) -> Response<Body> {
     let is_logged_in = is_logged_in(&ctx, &jar);
-    let search_form = search_form();
     let q = search_query.q.clone().unwrap_or_default();
-    let results = search(&ctx, &q).await;
+    let search_form = search_form(&q);
+    let mut results = search(&ctx, &q).await;
     let results = results
-        .iter()
-        .map(|r| {
+        .iter_mut()
+        .map(|p| {
+            crate::md::sanitize_preview(p);
+            let is_front_page_preview = true;
+            let html = post_to_html(p, is_front_page_preview);
             format!(
                 "
-                <div>
-                    <h2>{}</h2>
-                    <p>{}</p>
-                </div>
+                {html}
                 ",
-                r.id, r.content
             )
         })
         .collect::<Vec<_>>();
     let results = results.join("\n");
     let body = format!(
         "
-    {search_form}
-    <p>Query: {}
-    {results}
-    ",
-        q
+        {search_form}
+        {results}
+        "
     );
     let mut headers = HeaderMap::new();
     content_type(&mut headers, "text/html");
     let title = "Search";
     let extra_head = "";
-    let settings = PageSettings::new(&title, is_logged_in, false, Top::GoHome, extra_head);
+    let settings = PageSettings::new(title, is_logged_in, false, Top::GoHome, extra_head);
     let body = page(&ctx, &settings, &body).await;
     response(StatusCode::OK, headers, body, &ctx)
 }
