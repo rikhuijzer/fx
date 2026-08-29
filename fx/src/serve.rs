rikhuijzer/fx

commit 92ff091e21b99d93e7c9bb7f66573a99a556f85d

fix: h2-h6 headings causing infinite redirect loop

fx/src/serve.rs
@@ -675,6 +675,25 @@ pub struct AddPostForm {
     pub content: String,
 }
 
+/// H2-H6 headings (for example, `## Heading`) cause problems since the hash
+/// ends up into the Location header and handled incorrectly by the browser.  To
+/// solve, we just replace multiple hashes at the start of the string with a
+/// single hash.
+fn fix_invalid_heading_issue_179(content: &str) -> String {
+    let number_of_hashes = content.chars().take_while(|c| *c == '#').count();
+    if number_of_hashes == 0 {
+        content.to_string();
+    }
+    format!("#{}", &content[number_of_hashes..])
+}
+
+#[test]
+fn test_fix_invalid_heading_issue_179() {
+    let content = "### Heading";
+    let fixed = fix_invalid_heading_issue_179(content);
+    assert_eq!(fixed, "# Heading");
+}
+
 async fn post_add(
     State(ctx): State<ServerContext>,
     jar: CookieJar,
@@ -706,6 +725,7 @@ async fn post_add(
     if publish {
         let now = Utc::now();
         let content = trim_newline_suffix(&form.content);
+        let content = fix_invalid_heading_issue_179(&content);
         let post_id = Post::insert(&ctx.conn(), now, now, &content);
         if let Err(_e) = post_id {
             return response(

fx/src/files.rs
@@ -155,23 +155,6 @@ impl File {
     }
 }
 
-pub fn file_ext(file: &File) -> String {
-    let filename = &file.filename;
-    if file.mime_type.starts_with("image/") {
-        // For images, the extension is not important since the download link
-        // will automatically add the extension based on the mime type.
-        "".to_string()
-    } else {
-        // For other files, the extension is important since the browser cannot
-        // guess the extension based on the mime type.
-        if let Some(ext) = filename.split('.').next_back() {
-            format!(".{ext}")
-        } else {
-            "".to_string()
-        }
-    }
-}
-
 fn md_link(file: &File) -> String {
     let sha = &file.sha;
     let filename = &file.filename;

commit e338a1a14f7b29c4d1c2d5024eb94e2bb86187c2

fix: avoid adding hash when there is none (#196)
A bug was introduced in
https://github.com/rikhuijzer/fx/commit/92ff091e21b99d93e7c9bb7f66573a99a556f85d
that caused a hash to be added before the text when there was none. This
broke the blog post without title functionality.

Fixes #195

fx/src/serve.rs
@@ -682,16 +682,23 @@ pub struct AddPostForm {
 fn fix_invalid_heading_issue_179(content: &str) -> String {
     let number_of_hashes = content.chars().take_while(|c| *c == '#').count();
     if number_of_hashes == 0 {
-        content.to_string();
+        return content.to_string();
     }
     format!("#{}", &content[number_of_hashes..])
 }
 
 #[test]
-fn test_fix_invalid_heading_issue_179() {
+fn test_fix_invalid_heading_issue_179_reduces() {
     let content = "### Heading";
     let fixed = fix_invalid_heading_issue_179(content);
     assert_eq!(fixed, "# Heading");
+}
+
+#[test]
+fn test_fix_invalid_heading_issue_179_does_not_always_add_hash() {
+    let content = "Heading";
+    let fixed = fix_invalid_heading_issue_179(content);
+    assert_eq!(fixed, "Heading");
 }
 
 async fn post_add(